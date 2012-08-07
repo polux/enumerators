c46e40ba583baea3cf8c81e05532c65091693425
Paul Brauner <polux@google.com>
2012-08-07 09:20:42 +0200

    sync comments with main page

--- src/demo.dart
+++ src/demo.dart
@@ -51,12 +51,21 @@ fork(t1) => (t2) => new Fork(t1, t2);
 /** demos **/
 
 predefinedCombinators() {
+  // ideally, Combinators is a module but the VM doesn't support lazy
+  // initialization of toplevel variables yet
   final c = new Combinators();
+  // strings is an enumeration: a infinite list of finite parts
+  // part 0 contains the strings of size 0, part 1 the strings of size 1, etc.
   final strings = c.strings.parts[20];
+  // we have fast access to the cardinal of a part
   final n = (strings.card * 0.123).toInt();
+  // as well as fast access to the nth element of a part
   print("the ${n}th string of size 20: ${strings[n]}");
+  // we can also print a part as a whole, but it might be huge
   print("the ints of size 200: ${c.ints.parts[200]}");
+  // setsOf is a combinator: it takes an Enumeration and returns an Enumeration
   print("a set of strings: ${c.setsOf(c.strings)[123456789]}");
+  // we can abitrarily nest combinators
   print("a map from nats to lists of ints: "
         "${c.mapsOf(c.nats, c.listsOf(c.ints))[123456789]}");
 }
