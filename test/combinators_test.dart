1048b5bbe56cd21fd562b983186818786454af85
Paul Brauner <polux@google.com>
2012-11-05 13:58:04 +0100

    factor some code in the tests and add more tests

--- test/combinators_test.dart
+++ test/combinators_test.dart
@@ -14,32 +14,79 @@
 
 // Author: Paul Brauner (polux@google.com)
 
+import 'package:dart_enumerators/enumerators.dart' show Enumeration;
 import 'package:dart_enumerators/combinators.dart' as c;
 import 'package:unittest/unittest.dart';
 
-void checkNats() {
-  for (int n = 0; n < 500; n++) {
-    expect(c.nats.parts[n].card, equals(1),
-           reason: "part $n hasn't the expected cardinal");
-    expect(c.nats.parts[n][0], equals(n),
-           reason: "c.nats.parts[$n][0] isn't ${n}");
+void checkPrefix(Enumeration enum, List<List> prefix) {
+  for (int i = 0; i < prefix.length; i++) {
+    final finite = prefix[i];
+    final part = enum.parts[i];
+    expect(part.card, equals(finite.length));
+    for (int j = 0; j < finite.length; j++) {
+      expect(part[j], equals(finite[j]));
+    }
   }
 }
 
-void checkInts() {
-  expect(c.ints.parts[0].card, equals(1));
-  expect(c.ints.parts[0][0], equals(0));
-  for (int n = 1; n < 500; n++) {
-    expect(c.ints.parts[n].card, equals(2),
-           reason: "part $n hasn't the expected cardinal");
-    expect(c.ints.parts[n][0], equals(n),
-           reason: "c.ints.parts[$n][0] isn't ${n - 1}");
-    expect(c.ints.parts[n][1], equals(-n),
-           reason: "c.ints.parts[$n][1] isn't ${1 - n}");
+void checkUndefined(Enumeration enum, int from, int to) {
+  for (int i = from; i < to; i++) {
+    expect(() => enum.parts[i], throwsIndexOutOfRangeException);
   }
 }
+
+void testBools() {
+  checkPrefix(c.bools, [[true, false]]);
+  checkUndefined(c.bools, 1, 500);
+}
+
+void testNats() {
+  final expected = [];
+  for (int n = 0; n < 500; n++) {
+    expected.add([n]);
+  }
+  checkPrefix(c.nats, expected);
+}
+
+void testInts() {
+  final expected = [[0]];
+  for (int n = 1; n < 500; n++) {
+    expected.add([n, -n]);
+  }
+  checkPrefix(c.ints, expected);
+}
+
+void testListsOfBools() {
+  final enum = c.listsOf(c.bools);
+  final t = true, f = false;
+  final expected = [
+    [[]],
+    [[t], [f]],
+    [[t, t], [t, f], [f, t], [f, f]],
+    [[t, t, t], [t, t, f], [t, f, t], [t, f, f],
+     [f, t, t], [f, t, f], [f, f, t], [f, f, f]]];
+  checkPrefix(enum, expected);
+}
+
+void testListsOfNats() {
+  final enum = c.listsOf(c.nats);
+  final expected = [
+    [[]],
+    [[0]],
+    [[0, 0], [1]],
+    [[0, 0, 0], [0, 1], [1, 0], [2]],
+    [[0, 0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 2],
+     [1, 0, 0], [1, 1], [2, 0], [3]]];
+  checkPrefix(enum, expected);
+}
+
 
 main() {
-  test('nats is { 0: [], 1: [0], 2: [1], ... }', checkNats);
-  test('ints is { 0: [0], 1: [1, -1], 2: [2, -2], ... }', checkInts);
+  test('bools is { 0: [true, false], 1: [], 2: [], ... }', testBools);
+  test('nats is { 0: [], 1: [0], 2: [1], ... }', testNats);
+  test('ints is { 0: [0], 1: [1, -1], 2: [2, -2], ... }', testInts);
+  test('listsOf(bools) is { 0: [[]], 1: [[true], [false]], .. }',
+       testListsOfBools);
+  test('listsOf(nats) is { 0: [[0]], 1: [[0, 0], [1]], .. }',
+       testListsOfNats);
 }
