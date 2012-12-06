98dc98a1931e3d8734b07c3f14ea55a129780e15
polux <polux@google.com>
2012-12-06 01:57:26 +0100

    switch to dart license

--- example/lambda.dart
+++ example/lambda.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2012, Google Inc. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:dart_enumerators/enumerators.dart' as e;
 
 e.Finite singleton(x) => new e.Finite.singleton(x);
