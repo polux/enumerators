8e29c873718dc687b9ece6989e910c11b8ad8a30
Paul Brauner <polux@google.com>
2013-10-03 01:48:54 +0200

    remove hints in run_all_tests.sh

--- tool/run_all_tests.sh
+++ tool/run_all_tests.sh
@@ -2,9 +2,9 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-dartanalyzer $ROOT_DIR/lib/*.dart \
-&& dartanalyzer $ROOT_DIR/test/*.dart \
-&& dartanalyzer $ROOT_DIR/example/*.dart \
+dartanalyzer --no-hints $ROOT_DIR/lib/*.dart \
+&& dartanalyzer --no-hints $ROOT_DIR/test/*.dart \
+&& dartanalyzer --no-hints $ROOT_DIR/example/*.dart \
 && dart --enable-checked-mode $ROOT_DIR/example/simple.dart \
 && dart --enable-checked-mode $ROOT_DIR/example/advanced.dart \
 && dart --enable-checked-mode $ROOT_DIR/example/lambda.dart \
