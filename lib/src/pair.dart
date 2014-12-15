29df2f0c107399165e36b6099a17b255f24ce72c
Paul Brauner <polux@google.com>
2014-12-15 20:08:07 +0100

    cosmetic changes

--- lib/src/pair.dart
+++ lib/src/pair.dart
@@ -3,13 +3,20 @@ part of enumerators;
 class Pair<A,B> {
   final A fst;
   final B snd;
+
   Pair(this.fst, this.snd);
+
   Pair<A,B> setFst(A x) => new Pair<A,B>(x, snd);
+
   Pair<A,B> setSnd(B x) => new Pair<A,B>(fst, x);
+
   int get hashCode => 31 * fst.hashCode + snd.hashCode;
-  bool operator ==(Pair<A,B> other) =>
-      other is Pair<A,B>
-      && fst == other.fst
-      && snd == other.snd;
+
+  bool operator ==(Pair<A,B> other) {
+    return (other is Pair<A,B>)
+        && (fst == other.fst)
+        && (snd == other.snd);
+  }
+
   toString() => "($fst, $snd)";
 }
